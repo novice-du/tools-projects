44b840954836754168f70c8971e27d86174d3a46
DJH <18595924230@163.com>
2021-11-03 15:40:18 +0800

    新增判断某个坐标点是否在不规则范围内

--- tools-projects-new/algorithm/src/main/java/com/djh/lonAndLat/circle/InCircle.java
+++ tools-projects-new/algorithm/src/main/java/com/djh/lonAndLat/circle/InCircle.java
@@ -1,4 +1,4 @@
-package com.djh.inCircle;
+package com.djh.lonAndLat.circle;
 
 import com.vividsolutions.jts.algorithm.PointLocator;
 import com.vividsolutions.jts.geom.Coordinate;
@@ -31,7 +31,8 @@ public class InCircle {
 
         GeometryFactory gf=new GeometryFactory();
         Geometry gfLineString = gf.createLineString(coordinates4);
-        double degree = dis / (2*Math.PI*6371004)*360; //将度换算成米，公式为：degree = meter / (2 * Math.PI * 6371004) * 360
+        //将度换算成米，公式为：degree = meter / (2 * Math.PI * 6371004) * 360
+        double degree = dis / (2*Math.PI*6371004)*360;
         //缓冲区建立
         BufferOp bufOp = new BufferOp(gfLineString);
         bufOp.setEndCapStyle(BufferOp.CAP_BUTT);
---
+++ tools-projects-new/algorithm/src/main/java/com/djh/lonAndLat/polygon/Coordinate.java
@@ -0,0 +1,12 @@
+package com.djh.lonAndLat.polygon;
+
+
+/**
+ * @author: dujunhua
+ * @create: 2021/11/03 15:10
+ * @description: 判断一个坐标点是否在不规则范围内
+ */
+public class Coordinate {
+
+
+}
